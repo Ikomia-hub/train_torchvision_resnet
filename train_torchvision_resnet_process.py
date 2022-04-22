1307294f1598d65a2d716b1874f88b047a5afda0
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:21 +0200

    API change for getProgressSteps() function

--- train_torchvision_resnet_process.py
+++ train_torchvision_resnet_process.py
@@ -67,7 +67,7 @@ class TrainResnet(dnntrain.TrainProcess):
         self.resnet = Resnet(self.getParam())
         self.enableTensorboard(False)
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         param = self.getParam()

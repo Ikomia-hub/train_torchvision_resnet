cd83dda412120480f4ce5f7df210de2c73f1dc39
ambroise <ambroise.berthe@ikomia.com>
2023-08-04 11:34:46 +0200

    Fix test file

--- train_torchvision_resnet_test.py
+++ train_torchvision_resnet_test.py
@@ -1,9 +1,5 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
-import os
-import ikomia
-import yaml
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +7,9 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train torchvision resnet =====")
     input_path = t.get_input(0)
-    params = task.get_parameters(t)
+    params = t.get_param_object()
     params["epochs"] = 1
     params["batch_size"] = 2
-    task.set_parameters(t, params)
+    t.set_parameters(params)
     input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
     yield run_for_test(t)

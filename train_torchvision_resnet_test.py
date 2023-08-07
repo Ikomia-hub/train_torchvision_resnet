972350021ed5adbf79da2fc0c4414d6430221b6e
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 10:18:23 +0200

    Fix test file

--- train_torchvision_resnet_test.py
+++ train_torchvision_resnet_test.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train torchvision resnet =====")
     input_path = t.get_input(0)
-    params = t.get_param_object()
-    params["epochs"] = 1
-    params["batch_size"] = 2
+    params = t.get_parameters()
+    params["epochs"] = "1"
+    params["batch_size"] = "2"
     t.set_parameters(params)
     input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
     yield run_for_test(t)
